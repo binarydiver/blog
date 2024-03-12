8b6358265ab950e735433aa8e579041b41233999
Jason Park <jason@binarydiver.com>
2024-03-12 15:04:42 +0900

    fix base path

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/template-nextjs-blog' : '',
+  // basePath: isProd ? '/template-nextjs-blog' : '',
   images: {
     unoptimized: true,
   },
