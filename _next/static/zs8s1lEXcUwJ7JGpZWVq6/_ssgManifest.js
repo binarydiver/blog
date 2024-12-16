dae05bb1f0391d2c26c718d75e8991651416adc9
Jason Park <jason@binarydiver.com>
2024-12-16 22:03:45 +0900

    Updates

---
+++ _next/static/zs8s1lEXcUwJ7JGpZWVq6/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Farticles\u002F2024\u002F09\u002F24\u002Fbuild-blog-with-nextjs-on-github","\u002Farticles\u002F2024\u002F09\u002F25\u002Fappearance-feature-from-wikipedia","\u002Farticles\u002F2024\u002F12\u002F16\u002Feasily-set-up-my-own-vpn-server"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
